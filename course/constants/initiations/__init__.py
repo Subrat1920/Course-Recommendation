5e3bc38471a56bbd22ab35bbe95bf0c9afc04b54
Subrat1920 <3subratmishra1sep@gmail.com>
2025-04-14 01:54:36 +0530

    adding the component files

---
+++ course/constants/initiations/__init__.py
@@ -0,0 +1,22 @@
+'''
+DEFINIG THE ARTIFACTS DIRECTORY
+'''
+DATABASE_NAME:str='Course_Directory'
+
+'''
+DATA INGESTION
+'''
+DATA_INGESTION_TABLE_NAME:str='ingested_data'
+
+
+'''
+DATA VALIDATION
+'''
+DATA_VALIDATION_FOLDER_NAME:str='Data_Validation'
+DATA_VALIDATION_FILE_NAME:str='validate_report.txt'
+
+'''
+DATA TRANSFORMATION
+'''
+
+
