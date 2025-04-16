a135f62b178d4d7c173c771db43d8743a6375e30
Subrat1920 <3subratmishra1sep@gmail.com>
2025-04-16 07:34:52 +0530

    data transformation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from course.components.data_ingestion import DataIngestion
+from course.components.data_transformation.popularity_based_filtering import PopularityFiltering
 from course.exception.exception import RecommenderException, error_message_details
 from course.logger.logging import logging
 import sys
@@ -9,6 +10,11 @@ if __name__=='__main__':
         print("Data Ingestion Initiated")
         data_ingestion = DataIngestion()
         data_ingestion.initiate_data_ingestion()
+
+        ## intializing data transformation
+        print('Popularity Filtering Initated')
+        popularity_filtering = PopularityFiltering()
+        popularity_filtering.initiate_popularity_based_filtering()
     except Exception as e:
         logging.error(error_message_details(e, sys))
         raise RecommenderException(e,sys) from e
